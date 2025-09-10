674ae013627fb76490fcee76439086e37e9766d5
@coodersio <165970558+coodersio@users.noreply.github.com>
2025-09-10 20:40:52 +0800

    changsitemap

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/sitemap.xml',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'application/xml; charset=utf-8',
-          },
-        ],
-      },
-      {
-        source: '/robots.txt',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'text/plain; charset=utf-8',
-          },
-        ],
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;

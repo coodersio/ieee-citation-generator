bc20ff5e8213398b530c4f624d112704c645fa65
@coodersio <165970558+coodersio@users.noreply.github.com>
2025-09-10 20:10:40 +0800

    1

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
             userAgent: '*',
             allow: '/',
         },
-        sitemap: 'https://ieeecitationgenerator.com/sitemap.xml',
+        sitemap: 'https://ieeecitationgenerator.xyz/sitemap.xml',
     };
 }
